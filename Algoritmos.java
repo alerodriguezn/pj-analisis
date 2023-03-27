e21fe60ee1366aeada0e7a271bb8a1ef2c28791e
DeividMatuteG <deividmg02@gmail.com>
2023-03-26 22:50:14 -0600

    Asignaciones y archivos de texto

--- Algoritmos.java
+++ Algoritmos.java
@@ -3,6 +3,8 @@ import java.util.Arrays;
 
 public class Algoritmos {
 
+    public static int a = 0; //Asignaciones
+
     /*
      * Algoritmo de fuerza bruta
      */
@@ -10,10 +12,13 @@ public class Algoritmos {
         ArrayList<Integer> posiciones = new ArrayList<>();
         int n = texto.length();
         int m = patron.length();
+        a+=3;
 
         for (int i = 0; i <= n - m; i++) {
+            a+=2;
             int j;
             for (j = 0; j < m; j++) {
+                a+=2;
                 if (texto.charAt(i + j) != patron.charAt(j)) {
                     break;
                 }
@@ -33,22 +38,27 @@ public class Algoritmos {
         int n = texto.length();
         int m = patron.length();
         int[] pi = preprocesarPatron(patron);
+        a+=4;
 
         int i = 0;
         int j = 0;
+        a+=2;
         while (i < n) {
             if (texto.charAt(i) == patron.charAt(j)) {
                 i++;
-                j++;
+                j++;a+=2;
                 if (j == m) {
                     posiciones.add(i - j);
                     j = pi[j - 1];
+                    a++;
                 }
             } else {
                 if (j == 0) {
                     i++;
+                    a++;
                 } else {
                     j = pi[j - 1];
+                    a++;
                 }
             }
         }
@@ -61,17 +71,21 @@ public class Algoritmos {
         pi[0] = 0;
         int j = 0;
         int i = 1;
+        a+=5;
         while (i < m) {
             if (patron.charAt(i) == patron.charAt(j)) {
                 j++;
                 pi[i] = j;
                 i++;
+                a+=3;
             } else {
                 if (j == 0) {
                     pi[i] = 0;
                     i++;
+                    a+=2;
                 } else {
                     j = pi[j - 1];
+                    a++;
                 }
             }
         }
@@ -84,36 +98,41 @@ public class Algoritmos {
      */
     public static ArrayList<Integer> algoritmoBoyerMoore(String texto, String patron) {
         ArrayList<Integer> posiciones = new ArrayList<>();
+        int[] tablaSalto = crearTablaSalto(patron);
         int n = texto.length();
         int m = patron.length();
-        int[] tablaSalto = crearTablaSalto(patron);
-
         int i = m - 1;
-        int j = m - 1;
+        a+=5;
         while (i < n) {
-            if (texto.charAt(i) == patron.charAt(j)) {
-                if (j == 0) {
-                    posiciones.add(i);
-                    i += m;
-                    j = m - 1;
-                } else {
-                    i--;
-                    j--;
-                }
+            int k = i;
+            int j = m - 1;
+            a+=2;
+            while (j >= 0 && texto.charAt(k) == patron.charAt(j)) {
+                j--;
+                k--;
+                a+=2;
+            }
+            if (j == -1) {
+                posiciones.add(k + 1);
+                i++;
+                a++;
             } else {
-                i += tablaSalto[texto.charAt(i)];
-                j = m - 1;
+                i += Math.max(tablaSalto[texto.charAt(i)], m - j - 1);
+                a++;
             }
         }
-        return posiciones;
+    return posiciones;
     }
 
     private static int[] crearTablaSalto(String patron) {
         int m = patron.length();
         int[] tablaSalto = new int[256];
+        a+=2;
         Arrays.fill(tablaSalto, m);
+        a++;
         for (int i = 0; i < m - 1; i++) {
             tablaSalto[patron.charAt(i)] = m - i - 1;
+            a++;
         }
         return tablaSalto;
     }
--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Main{
 
@@ -11,11 +14,10 @@ public class Main{
         if (posiciones.size() == 0) {
             System.out.println("El patrón no se encontró en el texto.");
         } else {
-            System.out.println("El patrón se encontró en las siguientes posiciones:");
-            for (int i = 0; i < posiciones.size(); i++) {
-                System.out.println(posiciones.get(i));
-            }
+            System.out.println("El patrón se encontró en las siguientes posiciones: "+posiciones.toString());
         }
+        System.out.println("Cantidad de Asignaciones: " + Algoritmos.a);
+        Algoritmos.a = 0;
     }
 
     public static void main(String[] args) {
@@ -23,6 +25,35 @@ public class Main{
         // Texto y patrón de prueba
         String texto = "AABAACAADAABAABA";
         String patron = "AABA";
+
+        //Texto con base a los archivos (prueba)
+        String rutaArchivo = "texto1.txt"; 
+        File archivo = new File(rutaArchivo);
+        long tamanoBytes = archivo.length();
+
+        //Lectura del texto del archivo
+        Scanner scanner;
+        String contenido = ""; //Tendrá el texto del archivo
+        if (archivo.exists() && archivo.canRead()) { //verifica si el archivo existe y si el programa tiene permiso para leerlo
+            try {
+                scanner = new Scanner(archivo);
+                while (scanner.hasNextLine()) {
+                    contenido += scanner.nextLine();
+                }    
+                scanner.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("Surgió un error al intentar leer el archivo");
+            }
+        } else {
+            System.out.println("El archivo no existe o no se puede leer");
+        }
+
+
+        //Establezca el texto basado en el texto del archivo
+        texto = contenido;
+        
+
+        System.out.println("El tamaño del archivo es de " + tamanoBytes + " bytes.");
 
         // Algoritmo Fuerza Bruta
         ArrayList<Integer> posicionesFB = Algoritmos.algoritmoFuerzaBruta(texto, patron);
