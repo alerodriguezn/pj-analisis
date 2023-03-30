14172a47943d51aacb38c40c45622edc421391f0
alerodriguezn <navarrocmo@gmail.com>
2023-03-30 13:46:45 -0600

    Patrones

--- Main.java
+++ Main.java
@@ -1,21 +1,38 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Main {
 
     /*
      * Imprime el resultado del algoritmo
      */
-    public static void imprimirResultado(ArrayList<Integer> posiciones, String nombreAlgoritmo) {
+    public static void imprimirResultado(ArrayList<Integer> posiciones, String nombreAlgoritmo, String patron) {
         System.out.println("-------------------------------------");
         System.out.println("Nombre del algoritmo: " + nombreAlgoritmo);
+        System.out.println("Patrón: " + patron);	
         if (posiciones.size() == 0) {
             System.out.println("El patrón no se encontró en el texto.");
         } else {
             System.out.println("El patrón se encontró en las siguientes posiciones: " + posiciones.toString());
         }
+
+        // Almacena los resultados en un archivo de texto 
+        try {
+            PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter("archivo.txt", true)));
+            String linea = nombreAlgoritmo + "," + patron + "," + Algoritmos.a + "," + Algoritmos.c;
+            writer.println(linea);
+            writer.close();
+        } catch (IOException e) {
+            System.err.println("Error al escribir en el archivo de texto plano: " + e.getMessage());
+        }
+    
+
         System.out.println("Cantidad de Asignaciones: " + Algoritmos.a);
         Algoritmos.a = 0;
         System.out.println("Cantidad de Comparaciones: " + Algoritmos.c);
@@ -24,7 +41,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String patron = "AABA";
+        String[] patrones = { "ABCDEF", "ABCDEFGHIJKLNMOPQRSTOVXYZ", "ZZZZZ", "ZZZZZZZZZZZZZZZZZZZZZZZZZ", "ABABABA", "ABABABABABABABABABABABABA" };
 
         for (int i = 1; i <= 6; i++) {
             // Texto con base a los archivos (prueba)
@@ -56,17 +73,20 @@ public class Main {
             System.out.println(".............................................................................");
             System.out.println("El tamaño del archivo " + rutaArchivo + " es de " + tamanoBytes + " bytes.");
 
-            // Algoritmo Fuerza Bruta
-            ArrayList<Integer> posicionesFB = Algoritmos.algoritmoFuerzaBruta(texto, patron);
-            imprimirResultado(posicionesFB, "Fuerza bruta");
+            for (String patron : patrones) {
+                ArrayList<Integer> posicionesFB = Algoritmos.algoritmoFuerzaBruta(texto, patron);
+                imprimirResultado(posicionesFB, "Fuerza bruta" , patron);
 
-            // Algoritmo Knuth-Morris-Pratt
-            ArrayList<Integer> posicionesKMP = Algoritmos.algoritmoKnuthMorrisPratt(texto, patron);
-            imprimirResultado(posicionesKMP, "Knuth-Morris-Pratt");
+                // Algoritmo Knuth-Morris-Pratt
+                ArrayList<Integer> posicionesKMP = Algoritmos.algoritmoKnuthMorrisPratt(texto, patron);
+                imprimirResultado(posicionesKMP, "Knuth-Morris-Pratt", patron);
 
-            // Algoritmo Boyer-Moore
-            ArrayList<Integer> posicionesBM = Algoritmos.algoritmoBoyerMoore(texto, patron);
-            imprimirResultado(posicionesBM, "Boyer-Moore");
+                // Algoritmo Boyer-Moore
+                ArrayList<Integer> posicionesBM = Algoritmos.algoritmoBoyerMoore(texto, patron);
+                imprimirResultado(posicionesBM, "Boyer-Moore", patron);
+
+            }
+
         }
 
     }
